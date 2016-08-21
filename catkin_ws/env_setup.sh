7a5ca3c61b937334dd4a7ec41dfcfa9214b0da46
Michael Huang <bbxxtt2010@gmail.com>
2016-08-21 18:42:26 -0500

    reuse menge_common and set menge project in world file
    project file can be overriden in world file as parameter to plugins
    by default it still uses MENGE_PROJECT_PATH env if not set in world file
    
    MENGE_ROOT is the only env var now required, pointing to root dir of Menge tree

--- catkin_ws/env_setup.sh
+++ catkin_ws/env_setup.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 current_dir=`pwd`
-menge_root=$current_dir/../Menge
-export MENGE_EXE=$menge_root/Exe
-export MENGE_PROJECT_PATH=$menge_root/examples/core/outside.xml
+export MENGE_ROOT=$current_dir/../Menge
+export MENGE_PROJECT_PATH=examples/core/outside.xml
 export GAZEBO_PLUGIN_PATH=$current_dir/devel/lib/
 export GAZEBO_MODEL_PATH=$current_dir/src/menge_gazebo/menge_gazebo_worlds/models
-cp $MENGE_EXE/libmengeCore.so devel/lib/
+cp $MENGE_ROOT/Exe/libmengeCore.so devel/lib/
