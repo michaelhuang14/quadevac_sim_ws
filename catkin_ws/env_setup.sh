c863986ac87e1cbd67375635f275a6f202280da3
Michael Huang <bbxxtt2010@gmail.com>
2016-09-10 16:05:50 -0500

    fine tune for case study

--- catkin_ws/env_setup.sh
+++ catkin_ws/env_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 current_dir=`pwd`
 export MENGE_ROOT=$current_dir/../Menge
-export MENGE_PROJECT_PATH=examples/core/outside.xml
+export MENGE_PROJECT_PATH=examples/core/predetermined.xml
 export GAZEBO_PLUGIN_PATH=$current_dir/devel/lib/
 export GAZEBO_MODEL_PATH=$current_dir/src/menge_gazebo/menge_gazebo_worlds/models
 cp $MENGE_ROOT/Exe/libmengeCore.so devel/lib/
